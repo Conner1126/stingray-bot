e797ba82be11b3e9f920de1182e7ab61a6c4fdfc
Conner1126 <57330720+Conner1126@users.noreply.github.com>
2025-10-01 18:58:26 -0400

    delete unused controller connect

--- controller_connect.sh
+++
@@ -1,8 +0,0 @@
-bluetoothctl
-power on
-agent on
-scan on
-# Wait for your Xbox controller to appear, note its MAC address
-pair 44:16:22:C5:E1:69
-connect 44:16:22:C5:E1:69
-trust 44:16:22:C5:E1:69
